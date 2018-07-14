161322acd4219ada7bc7a207ff3ac46d97f7d96b
mminhtun <mhtun@interwebbingtechnology.com>
2018-07-14 14:12:38 +0630

    Room CRUD Sample

---
+++ app/src/main/java/com/ehdosoe/newmac6/roomwordssample/Word.java
@@ -0,0 +1,38 @@
+package com.ehdosoe.newmac6.roomwordssample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    @Ignore
+    public Word(int id, @NonNull String word) {
+        this.id = id;
+        this.mWord = word;
+    }
+
+    public Word(@NonNull String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getWord() {
+        return this.mWord;
+    }
+
+    public int getId() {return id;}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
